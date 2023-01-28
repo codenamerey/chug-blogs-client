e73ce1ea97cfa120fc388b93a9f02163409d0e99
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-28 16:35:41 +0800

    Include Form in login component

---
+++ src/pages/login.js
@@ -0,0 +1,22 @@
+import Form from "@/components/Form"
+
+const login = () => {
+  return (
+    <main className="flex flex-col justify-center items-center grow">
+        <Form>
+            <label htmlFor="first_name">First Name: </label>
+            <input type="text" id="first_name" name="first_name" />
+
+            <label htmlFor="last_name">Last Name: </label>
+            <input type="text" id="last_name" name="last_name" />
+
+            <label htmlFor="password">Password: </label>
+            <input type="text" id="password" name="password" />
+
+            <input type="submit" value="Log In" className=" bg-sky-900 px-6 py-3 rounded-full m-4 font-bold text-white inner shadow-md"/>
+        </Form>
+    </main>
+  )
+}
+
+export default login
