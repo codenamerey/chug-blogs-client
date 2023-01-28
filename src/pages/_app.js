6ce4fd72ebc825694c5aef2cd9de081ecdc34bd0
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-28 17:03:45 +0800

    Make body occupy space

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,11 @@ import Header from '@/components/Header'
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Header>
-    </Header>
-    <main className=' bg-gradient-to-tr from-orange-300 to-orange-500 grow min-h-screen flex flex-col'>
-      <Component {...pageProps}/>
-    </main>
+      <Header>
+      </Header>
+      <main className=' bg-gradient-to-tr from-orange-300 to-orange-500 grow min-h-full flex flex-col'>
+        <Component {...pageProps}/>
+      </main>
     </>
     )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='flex flex-col'>
+      <body className='flex flex-col h-screen'>
         <Main />
         <NextScript />
       </body>
