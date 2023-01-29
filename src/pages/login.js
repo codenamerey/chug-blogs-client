67d0cbe99d962319345a1d0206b8fa0d02256fc3
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-29 11:11:14 +0800

    Add email to log in form

--- src/pages/login.js
+++ src/pages/login.js
@@ -10,8 +10,11 @@ const login = () => {
             <label htmlFor="last_name">Last Name: </label>
             <input type="text" id="last_name" name="last_name" />
 
+            <label htmlFor="email">Email: </label>
+            <input type="text" id="email" name="email_address" />
+
             <label htmlFor="password">Password: </label>
-            <input type="text" id="password" name="password" />
+            <input type="password" id="password" name="password" />
 
             <input type="submit" value="Log In" className=" bg-sky-900 px-6 py-3 rounded-full m-4 font-bold text-white inner shadow-md"/>
         </Form>
