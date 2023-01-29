8d1f48dd63a6ce5125e9e7088bea63d976b24025
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-29 11:15:43 +0800

    Pass submit function to Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Form = ({ children }) => {
+const Form = ({ children, submitFunction }) => {
+
+
   return (
-    <form method="post" className='bg-white shadow-lg shadow-gray-700 flex flex-col items-center m-10 mt-0 md:w-1/2 md:m-0 [&>input[type="text"]]:outline-none [&>input[type="text"]]:border [&>input[type="text"]]:px-6 [&>input[type="text"]]:py-3 p-2 align-middle'>
+    <form id='login-form' onSubmit={submitFunction} method="post" className='bg-white shadow-lg shadow-gray-700 flex flex-col items-center m-10 mt-0 md:w-1/2 md:m-0 [&>input[type="text"]]:outline-none [&>input[type="text"]]:border [&>input[type="text"]]:px-6 [&>input[type="text"]]:py-3 p-2 align-middle [&>input[type="password"]]:outline-none [&>input[type="password"]]:border [&>input[type="password"]]:px-6 [&>input[type="password"]]:py-3'>
         {children}
     </form>
   )
