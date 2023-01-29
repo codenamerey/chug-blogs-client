8172fe9972a6a4a4d0e045bb8f40ffcfb4bb11c7
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-29 11:58:22 +0800

    Allow log in and overwrite application user

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,20 @@
 import '@/styles/globals.css'
 import Header from '@/components/Header'
+import { useState, createContext } from 'react'
 
 export default function App({ Component, pageProps }) {
+  let UserContext = createContext(null);
+  let [user, setUser] = useState(null);
+
   return (
     <>
-      <Header>
-      </Header>
-      <main className=' bg-gradient-to-tr from-orange-300 to-orange-500 grow min-h-full flex flex-col'>
-        <Component {...pageProps}/>
-      </main>
+      <UserContext.Provider value={{user, setUser}}>
+        <Header>
+        </Header>
+        <main className=' bg-gradient-to-tr from-orange-300 to-orange-500 grow min-h-full flex flex-col'>
+          <Component {...pageProps} UserContext={UserContext}/>
+        </main>
+      </UserContext.Provider>
     </>
     )
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,43 @@
 import Form from "@/components/Form"
+import { useContext } from "react";
 
-const login = () => {
+const login = ({ UserContext }) => {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleFormSubmit = async(e) => {
+    e.preventDefault();
+    const form = document.querySelector('#login-form');
+    const formData = new FormData(form);
+    const urlEncoded = new URLSearchParams(formData);
+    try {
+      let res = await fetch('http://localhost:4001/api/user/local/signin', {
+        method: 'post',
+        body: urlEncoded
+      }); 
+      let status = res.status;
+      if(status == 200) {
+        let data = await res.json();
+        localStorage.setItem('jwt-token', data.token);
+        setUser(data.user);
+        // Back to home page
+        window.location.href = '/';
+      }
+  
+      if(status == 422) {
+        let data = await res.json();
+        let message = data.message;
+        const msg = document.querySelector('#message');
+        msg.textContent = message;
+      }
+      
+    } catch(err) {
+      if(err) console.log(err);
+    }
+  }
+
   return (
     <main className="flex flex-col justify-center items-center grow">
-        <Form>
+        <Form submitFunction={handleFormSubmit}>
             <label htmlFor="first_name">First Name: </label>
             <input type="text" id="first_name" name="first_name" />
 
@@ -15,6 +49,8 @@ const login = () => {
 
             <label htmlFor="password">Password: </label>
             <input type="password" id="password" name="password" />
+
+            <p id="message"></p>
 
             <input type="submit" value="Log In" className=" bg-sky-900 px-6 py-3 rounded-full m-4 font-bold text-white inner shadow-md"/>
         </Form>
