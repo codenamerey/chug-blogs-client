0fe42e59d20c9f5154ec522540a18b019d5bbb8c
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-30 12:15:45 +0800

    Add link to individual articles

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import Link from "next/link";
 
 const Article = ({ article }) => {
     const {title, description} = article;
 
   return (
     <article className='p-2 shadow w-1/4 flex flex-col aspect-video justify-center items-center bg-gradient-to-tr from-orange-700 to-orange-900 text-white text-center'>
-        <h1 className=' text-2xl font-bold'>{title}</h1>
-
-        <h3 className=' text-xl'>{description}</h3>
+        <Link href={`/post/${article._id}`}>
+            <h1 className=' text-2xl font-bold'>{title}</h1>
+            <h3 className=' text-xl'>{description}</h3>
+        </Link>
     </article>
   )
 }
