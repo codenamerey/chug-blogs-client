73db15212e7ea98a92627cde89f1bdfd7813d4a9
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-30 08:43:16 +0800

    Add homepage link to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,10 +16,12 @@ const Header = ({ UserContext }) => {
 
   return (
     <header className='px-6 py-3 bg-gradient-to-tr from-orange-700 to-orange-900 flex justify-between items-center'>
-        <div className=' flex items-center gap-x-2'>
-            <img className='w-24' src="https://www.clipartmax.com/png/middle/65-659792_pixel-beer-glass-outlien.png" alt="beer" />
-            <h3 className='text-2xl font-bold text-white'>Chug <br /> Blogs</h3>
-        </div>
+        <Link href='/'>
+          <div className=' flex items-center gap-x-2'>
+              <img className='w-24' src="https://www.clipartmax.com/png/middle/65-659792_pixel-beer-glass-outlien.png" alt="beer" />
+              <h3 className='text-2xl font-bold text-white'>Chug <br /> Blogs</h3>
+          </div>
+        </Link>
         
         <nav>
           {
